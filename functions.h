e548419c4f6bd081ea538cc4c0df0f11085aa563
UESSENCE <165133889+UESSENCE@users.noreply.github.com>
2024-06-24 23:31:51 +0700

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+extern int verbose;
+
+void add_files_to_archive(const char* archive_path, const char* files_to_archive, int compression_level);
+void create_archive(const char* archive_path, const char* files_to_archive, int compression_level);
+void delete_files_from_archive(const char* archive_path, const char* files_to_archive);
+void extract_archive(const char* archive_path, const char* output_path);
+void list_archive(const char* archive_path);
