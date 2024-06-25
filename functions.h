82caac7e28f1fb72f8dcc895a551fde1b4d60586
arsithearsi <156746119+ArsenyyTheArsi@users.noreply.github.com>
2024-06-25 16:39:55 +0700

    Update functions.h

--- functions.h
+++ functions.h
@@ -2,10 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 extern int verbose;
 
+
+//Basic archiver functions
 void add_files_to_archive(const char* archive_path, const char* files_to_archive, int compression_level);
 void create_archive(const char* archive_path, const char* files_to_archive, int compression_level);
 void delete_files_from_archive(const char* archive_path, const char* files_to_archive);
 void extract_archive(const char* archive_path, const char* output_path);
 void list_archive(const char* archive_path);
+
+
+//To-be implemented compressio functions
+int encodeHuffman(FILE *inputFile, FILE *compressedFile);
+int decodeHuffman(FILE *compressedFile, FILE *outputFile, long long int originalFileSize);
